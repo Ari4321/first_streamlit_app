709a983cfdb3ae5cc745cc4daab8c8941fce889c
Aritra Kundu <114220171+Ari4321@users.noreply.github.com>
2023-05-15 14:13:25 +0530

    Update streamlit_app.py
    Added Manu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('I love fried Momos')
+streamlit.header('Breakfast Menu')
+streamlit.text('Chicken Fried Momos')
+streamlit.text('Price -100-6 pieces')
+streamlit.text('Afghani Tandoori Chicken fired Momos')
+streamlit.text('Price -120-6 pieces')
+
