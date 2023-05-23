be25552c3b83a2cb7b8ec32bb4bb05ca583c6b9f
Aritra Kundu <114220171+Ari4321@users.noreply.github.com>
2023-05-23 14:03:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Favourites')
+streamlit.header('Breakfast Favourites') 
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
